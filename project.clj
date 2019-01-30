cc9ae89e2a0f1eeff420d2eb6522bdf72e42cea3
Eric Normand <ericwnormand@gmail.com>
2019-01-30 16:17:26 -0600

    Update Clojure version

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "https://purelyfunctional.tv/"
   :license {:name "CC0 1.0 Universal (CC0 1.0) Public Domain Dedication"
             :url "http://creativecommons.org/publicdomain/zero/1.0/"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-time "0.12.2"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [clj-time "0.15.0"]])
